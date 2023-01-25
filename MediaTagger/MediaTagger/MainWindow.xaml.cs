3e9ae4b81ab830020a419538a189018de8d975b4
Matthew Covey <90411582+W0246645@users.noreply.github.com>
2023-01-25 01:05:02 -0400

    UI is mostly fleshed out, need to do the tag editor now.

--- MediaTagger/MediaTagger/MainWindow.xaml.cs
+++ MediaTagger/MediaTagger/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +29,25 @@ namespace MediaTagger
             InitializeComponent();
         }
 
+        private void ShowAlbumArt()
+        {
+            var pic = currentFile.Tag.Pictures[0];
+            if (pic != null)
+            { //Found code for this on 
+                MemoryStream ms = new MemoryStream(pic.Data.Data);
+                ms.Seek(0, SeekOrigin.Begin);
+
+                // ImageSource for System.Windows.Controls.Image
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.StreamSource = ms;
+                bitmap.EndInit();
+
+                // Create a System.Windows.Controls.Image control
+                albumImage.Source = bitmap;
+            }
+        }
+
         private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog fileDlg = new OpenFileDialog();
@@ -37,12 +57,23 @@ namespace MediaTagger
             //By default it returns ture if the user selects a file and hits open.
             if (fileDlg.ShowDialog() == true)
             {
-                fileNameBox.Text = fileDlg.FileName;
-
                 //Create tag lib file object, for accessing mp3 meta data.
                 currentFile = TagLib.File.Create(fileDlg.FileName);
 
                 myMediaPlayer.Source = new Uri(fileDlg.FileName);
+                myMediaPlayer.Play();
+
+                tagButton.IsEnabled = true;
+                menuPauseButton.IsEnabled = true;
+                pauseButton.IsEnabled = true;
+                menuStopButton.IsEnabled = true;
+                stopButton.IsEnabled = true;
+
+                ShowAlbumArt();
+
+                infoTitle.Text = currentFile.Tag.Title;
+                infoArtist.Text = currentFile.Tag.Artists.Any() ? currentFile.Tag.Artists[0] : "Unknown Artist.";
+                infoAlbum.Text = currentFile.Tag.Album + " (" + currentFile.Tag.Year + ")"; 
             }
         }
 
@@ -53,9 +84,38 @@ namespace MediaTagger
             {
                 var year = currentFile.Tag.Year;
                 var title = currentFile.Tag.Title;
-
-                tagNameBox.Text = title + " : " + year;
             }
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+            myMediaPlayer.Play();
+            pauseButton.IsEnabled = true;
+            menuPauseButton.IsEnabled = true;
+            stopButton.IsEnabled = true;
+            menuStopButton.IsEnabled = true;
+            playButton.IsEnabled = false;
+            menuPlayButton.IsEnabled = false;
+        }
+
+        private void pauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            myMediaPlayer.Pause();
+            playButton.IsEnabled = true;
+            menuPlayButton.IsEnabled = true;
+            pauseButton.IsEnabled = false;
+            menuPauseButton.IsEnabled = false;
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            myMediaPlayer.Stop();
+            playButton.IsEnabled = true;
+            menuPlayButton.IsEnabled = true;
+            stopButton.IsEnabled = false;
+            menuStopButton.IsEnabled = false;
+            pauseButton.IsEnabled = false;
+            menuPauseButton.IsEnabled = false;
         }
     }
 }
